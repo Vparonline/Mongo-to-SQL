5bb25a89360677dabdc9d716633c6e61712ac389
Elchavopy <74346832+Vparonline@users.noreply.github.com>
2024-05-15 03:17:47 +0300

    Add files via upload

---
+++ _elchavopy.py
@@ -0,0 +1,32 @@
+import pymongo
+import pymysql
+
+mongo_client = pymongo.MongoClient("mongodb://localhost:27017/içşarabısiarabı") # Mongo Adresiniz
+mongodb_database = mongo_client["sectwist"]  # Veri Çekilecek Koleksiyon Adı
+mongodb_collection = mongodb_database["userapi"] # Verininin Çekileceği Koleksiyonun İçindeki Şemanın Adı
+
+sql_connection = pymysql.connect(host='localhost', # Aşağıyı MySQL Bilgileriniz İle Doldurun.
+                                 user='root',
+                                 password='',
+                                 database='sectwist',
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor)
+sql_cursor = sql_connection.cursor()
+
+belgeler = mongodb_collection.find()
+
+for belge in belgeler:
+    if "UserIp" in belge:                
+        sql_cursor.execute("INSERT INTO verilerim (UserID, UserInformation, UserCreatedAt, UserGuildID, UserIP) VALUES (%s, %s, %s, %s, %s)",
+                            (belge["UserID"], belge["UserInformation"], belge["UserCreatedAt"], belge["UserGuildID"], belge["UserIp"]))
+    else:
+        sql_cursor.execute("INSERT INTO verilerim (UserID, UserInformation, UserCreatedAt, UserGuildID) VALUES (%s, %s, %s, %s)",
+                            (belge["UserID"], belge["UserInformation"], belge["UserCreatedAt"], belge["UserGuildID"]))
+
+    sql_connection.commit()
+
+    print("Yeni veri eklendi:")
+    print(belge)
+
+sql_cursor.close()
+sql_connection.close()
